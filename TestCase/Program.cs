3ea484a905728dfd247c2fafaf644e0611617b25
Ruder <cpc15@mail.ru>
2023-01-05 15:15:47 +0700

    Сделал перебор заполненых ячеек и вывод в консоль

--- TestCase/Program.cs
+++ TestCase/Program.cs
@@ -1,12 +1,32 @@
-﻿
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using ClosedXML.Excel;
+using TestCase.ExcelHelper;
+
 
 namespace TestCase
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            var path = "тестовые данные.xlsx";
+            var book = new XLWorkbook(path);
+            var sheet = book.Worksheet("Лист1");
+            var fistColumnNumber = sheet.FirstColumnUsed().ColumnNumber();
+            var lastColumnNumber = sheet.LastColumnUsed().ColumnNumber();
+            var firstRowNumber = sheet.FirstRowUsed().RowNumber();
+            var lastRowNumber = sheet.LastRowUsed().RowNumber();
+
+            var range = sheet.Range(firstRowNumber, fistColumnNumber, lastRowNumber, lastColumnNumber);
+            var cells = range.Cells();
+            foreach (var cell in cells)
+            {
+                Console.WriteLine(cell.GetString());
+                if (cell.GetString() == ProcessOwner.CORP.ToString())
+                {
+                    int corp = (int) ProcessOwner.CORP;
+                }
+            }
         }
     }
 }
